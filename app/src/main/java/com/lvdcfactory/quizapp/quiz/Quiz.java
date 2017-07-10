90649c118b5283a365d76dc07d7fdccecafa134c
Lu-Vuong <lu-vuongle@hotmail.com>
2017-07-10 22:01:46 +1000

    Created Quiz Class and Quiz Package

---
+++ app/src/main/java/com/lvdcfactory/quizapp/quiz/Quiz.java
@@ -0,0 +1,45 @@
+package com.lvdcfactory.quizapp.quiz;
+
+import com.lvdcfactory.quizapp.questions.Question;
+
+import java.util.List;
+
+/**
+ * Created by LU-VUONG on 10-Jul-17.
+ */
+
+public class Quiz {
+
+    private String author;
+    private String createdAt;
+    private String title;
+    private String description;
+    private List<Question> quizQuestions;
+
+    public Quiz(String author, String createdAt, String title, String description){
+        this.author = author;
+        this.createdAt = createdAt;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int numberOfQuestions(){
+        return quizQuestions.size();
+    }
+}
