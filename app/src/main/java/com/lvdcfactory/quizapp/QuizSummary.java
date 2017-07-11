0fbecaf3b6d117d37baeedfb0b488add5d192988
Lu-Vuong <lu-vuongle@hotmail.com>
2017-07-11 23:38:05 +1000

    Created Quiz Summary Activity with Basic Layout Design

---
+++ app/src/main/java/com/lvdcfactory/quizapp/QuizSummary.java
@@ -0,0 +1,13 @@
+package com.lvdcfactory.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_summary);
+    }
+}
