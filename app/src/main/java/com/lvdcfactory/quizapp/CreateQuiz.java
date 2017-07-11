b16c1a992c83f350e2acb4a59bfd51cf27e39cc2
Lu-Vuong <lu-vuongle@hotmail.com>
2017-07-11 14:41:23 +1000

    Added EventListener and Logic for Finding Button for Create Quiz Activity

--- app/src/main/java/com/lvdcfactory/quizapp/CreateQuiz.java
+++ app/src/main/java/com/lvdcfactory/quizapp/CreateQuiz.java
@@ -2,12 +2,33 @@ package com.lvdcfactory.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateQuiz extends AppCompatActivity {
+
+    Button createQuiz_btnCreateQuizContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quiz);
+
+        findViews();
+        addListeners();
+    }
+
+    private void findViews() {
+        createQuiz_btnCreateQuizContinue = (Button) findViewById(R.id.createQuiz_btnCreateQuizContinue);
+    }
+
+    private void addListeners(){
+        createQuiz_btnCreateQuizContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("CreateQuiz", "Continue Button Clicked");
+            }
+        });
     }
 }
