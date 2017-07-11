540c5ec4b88691f15a64de8556db06b85f5da678
Domenic Corso <domenic.corso@live.com>
2017-07-11 13:58:21 +1000

    Added essential Java code for finding buttons + adding listeners for the menu

--- app/src/main/java/com/lvdcfactory/quizapp/Menu.java
+++ app/src/main/java/com/lvdcfactory/quizapp/Menu.java
@@ -2,12 +2,44 @@ package com.lvdcfactory.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    Button btnCreateQuiz, btnViewYourQuizzes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        findViews();
+        addListeners();
     }
+
+    private void findViews() {
+        btnCreateQuiz = (Button) findViewById(R.id.menu_btnCreateQuiz);
+        btnViewYourQuizzes = (Button) findViewById(R.id.menu_btnViewYourQuizzes);
+    }
+
+    private void addListeners() {
+
+        btnCreateQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Menu", "'Create Quiz' Button Clicked");
+            }
+        });
+
+        btnViewYourQuizzes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Menu", "'View Your Quizzes' Button Clicked");
+            }
+        });
+
+    }
+
 }
