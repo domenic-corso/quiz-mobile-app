783e1899de10ae57d317337b5e838f0d40108ba5
Domenic Corso <domenic.corso@live.com>
2017-07-11 15:51:22 +1000

    More progress on 'AddQuestion' layout, the basic question insertion is there, next is to do multiple choice question insertion

--- app/src/main/java/com/lvdcfactory/quizapp/AddQuestion.java
+++ app/src/main/java/com/lvdcfactory/quizapp/AddQuestion.java
@@ -8,6 +8,6 @@ public class AddQuestion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_insert_question);
+        setContentView(R.layout.activity_add_question);
     }
 }
