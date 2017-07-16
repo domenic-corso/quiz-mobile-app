5fa2d760dbc063c498f180e96bcbbfe3be766fb4
Domenic Corso <domenic.corso@live.com>
2017-07-16 17:25:56 +1000

    Setup 'AddQuiz.java'

--- app/src/main/java/com/lvdcfactory/quizapp/PlayQuiz.java
+++ app/src/main/java/com/lvdcfactory/quizapp/PlayQuiz.java
@@ -2,12 +2,34 @@ package com.lvdcfactory.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class PlayQuiz extends AppCompatActivity {
+
+    TextView textViewQuestion;
+    ViewGroup layoutBasicQuestion, layoutMultipleChoice;
+    RadioGroup radioGroupMPA;
+    Button btnSubmitQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_quiz);
+
+        findViews();
+        addListeners();
     }
+
+    private void addListeners() {
+        // TODO add listeners for submit button
+    }
+
+    private void findViews() {
+        // TODO find these views
+    }
+
+    // TODO create event listener for the MPA radio buttons (inner class)
 }
