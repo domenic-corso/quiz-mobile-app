db3bceae011d15a3bcb7dbecb0a56c38d9719995
Lu-Vuong <lu-vuongle@hotmail.com>
2017-07-16 19:54:41 +1000

    Added Event Listeners for Start Test and Save Quiz Buttons

--- app/src/main/java/com/lvdcfactory/quizapp/QuizSummary.java
+++ app/src/main/java/com/lvdcfactory/quizapp/QuizSummary.java
@@ -3,9 +3,12 @@ package com.lvdcfactory.quizapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.lvdcfactory.quizapp.layout.PossibleAnswerLayoutWrapper;
 import com.lvdcfactory.quizapp.layout.QuestionSummaryLayoutWrapper;
 import com.lvdcfactory.quizapp.questions.Question;
 import com.lvdcfactory.quizapp.quiz.Quiz;
@@ -13,7 +16,7 @@ import com.lvdcfactory.quizapp.quiz.Quiz;
 public class QuizSummary extends AppCompatActivity {
 
     private Quiz newlyCreatedQuiz;
-
+    private Button btnStartTest, btnSaveTest;
     private TextView tvTitle, tvAuthor, tvDesc;
     private ViewGroup questionsContainer;
 
@@ -52,10 +55,30 @@ public class QuizSummary extends AppCompatActivity {
         tvAuthor = (TextView) findViewById(R.id.quizSummary_textViewAuthor);
         tvDesc = (TextView) findViewById(R.id.quizSummary_textViewDesc);
         questionsContainer = (ViewGroup) findViewById(R.id.quizSummary_questionsContainer);
+        btnStartTest = (Button) findViewById(R.id.quizSummary_btnStartTest);
+        btnSaveTest = (Button) findViewById(R.id.quizSummary_btnSave);
     }
 
     private void addListeners() {
+        btnStartTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showStartTestActivity();
+            }
+        });
 
+        btnSaveTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO write code to export quiz to a file
+            }
+        });
+    }
+
+    private void showStartTestActivity() {
+        Intent intent = new Intent(QuizSummary.this, PlayQuiz.class);
+        intent.putExtra(CreateQuiz.QUIZ_EXTRA, newlyCreatedQuiz);
+        startActivity(intent);
     }
 
     private void showQuestions() {
